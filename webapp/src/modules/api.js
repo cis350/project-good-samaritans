5d8fa1a3f30352f906d670a9691cbdfa61c7da92
Ethan Seto <ethanseto@Ethans-MacBook-Pro-4.local>
2022-04-22 10:56:09 -0400

    finished profile page web requests

---
+++ webapp/src/modules/api.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+
+const rootURL = 'http://localhost:10000';
+
+// profile page - sends request to change the privacy setting
+export async function changePrivacy(name) {
+  if (!name) {
+    throw new Error('no privacy setting');
+  }
+
+  try {
+    await axios.put(`${rootURL}/privacy`, { privacy: name });
+    return;
+  } catch (err) {
+    throw err;
+  }
+}
+
+// profile page - get the user's friends
+export async function getFriends(name) {
+  if (!name) {
+    throw new Error('no user given');
+  }
+
+  try {
+    const result = await axios.get(`${rootURL}/friends/${name}`);
+    return result;
+  } catch (err) {
+    throw err;
+  }
+}
+
+// profile page - gets the current list of help posts
+export async function getHelpPosts() {
+  try {
+    const result = await axios.get(`${rootURL}/help-posts`);
+    return result;
+  } catch (err) {
+    throw err;
+  }
+}
+
+// profile page - gets the current list of people you are currently helping
+export async function getSamaritanTexts(name) {
+  if (!name) {
+    throw new Error('no user given');
+  }
+
+  try {
+    const result = await axios.get(`${rootURL}/samaritan/${name}`);
+    return result;
+  } catch (err) {
+    throw err;
+  }
+}
+
