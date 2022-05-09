1fc32b5c9dcea03a5d84a25eaf692765a54f3397
Ethan Seto <ethanseto@Ethans-MacBook-Pro-4.local>
2022-05-09 15:25:26 -0700

    transferred eslintrc level

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,16 @@
 module.exports = {
   env: {
     browser: true,
+    commonjs: true,
     es2021: true,
+    jest: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
+    'airbnb-base',
   ],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 'latest',
-    sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
   rules: {
   },
 };
